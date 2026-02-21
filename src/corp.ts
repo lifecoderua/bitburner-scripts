95784eeb4e57f4ea09002fc63a181a2c272010af
Oleksii Volkov <lifecoder.ua@gmail.com>
2026-02-21 11:59:06 +0200

    corp - boost materials - wip

--- src/corp.ts
+++ src/corp.ts
@@ -24,13 +24,13 @@ export async function main(ns: NS) {
   // open Corp
   await setupCorp();
   
-  // TODO: Make it in stages
-  // TODO: Make stages declarative ([resource: count]) and idempotent (should survive restart).
-  await upgradeOfficeSize('Agro', 4);
+  await stage1();
 }
 
 async function setupCorp() {
   const ns: NS = Math.bb.ns;
+
+  ns.print(`hasCorp: ${ns.corporation.hasCorporation()}`)
 
   if (ns.corporation.hasCorporation()) {
     return;
@@ -39,7 +39,9 @@ async function setupCorp() {
   while (!ns.corporation.canCreateCorporation(true)) {
     await ns.sleep(10_000);
   }
+  ns.print('1')
   ns.corporation.createCorporation('GooCORP', true);
+  ns.print('2')
 }
 
 async function upgradeOfficeSize(divisionName: string, targetSize = 4) {
@@ -61,4 +63,64 @@ async function upgradeOfficeSize(divisionName: string, targetSize = 4) {
     ns.corporation.upgradeOfficeSize(divisionName, city, extraSize);
     ns.corporation.hireEmployee(divisionName, city);
   });
+}
+
+function expandAllCitites(divisionName: string) {
+  const ns: NS = Math.bb.ns;
+
+  const alreadyExpanded = ns.corporation.getDivision(divisionName).cities as CityName[];
+
+  cities
+    .filter((city) => !alreadyExpanded.includes(city))  
+    .forEach((city) => {
+      ns.corporation.expandCity(divisionName, city)
+    });
+}
+
+/**
+ * Lifecycle STAGES
+ * 
+ * Get to the desired state, stabilize, gather investments;
+ */
+
+async function stage1() {
+  const ns: NS = Math.bb.ns;
+  const divisionName = 'Agro';
+
+  // TODO: use check instead
+  try {
+    ns.corporation.expandIndustry('Agriculture', divisionName);  
+  } catch (e) {}
+  expandAllCitites(divisionName);
+
+  ensureBoostMaterials([
+    { name: 'Real Estate', amount: 10 },
+  ])
+  // TODO: Make it in stages
+  // TODO: Make stages declarative ([resource: count]) and idempotent (should survive restart).
+  
+  // TODO: !!! RE-ENABLE
+  // await upgradeOfficeSize(divisionName, 4);
+}
+
+// TODO: autoSupply / calc reqs, balance remains
+
+type BoostMaterialRequest = {
+  name: string,
+  amount: number,
+}
+
+function ensureBoostMaterials(boostMaterials: BoostMaterialRequest[]) {
+  const ns: NS = Math.bb.ns;
+  const divisionName = 'Agro';
+
+  boostMaterials.forEach((materialRequest) => {
+    cities.forEach((city) => {
+      const presentMaterial = ns.corporation.getMaterial(divisionName, city, materialRequest.name);
+      
+      if (presentMaterial.stored >= materialRequest.amount) { return; }
+
+      ns.corporation.buyMaterial(divisionName, city, materialRequest.name, materialRequest.amount / 10);
+    })
+  })
 }
